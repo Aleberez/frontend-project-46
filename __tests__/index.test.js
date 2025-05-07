6c0ba8283204abc12f3b80d63ef8eea2691c5c03
lazfnol <lazfnol@gmail.com>
2025-05-07 20:06:20 +0300

    add tests for yaml

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,3 +4,11 @@ import result from '../__fixtures__/result.js';
 test('check json stylish', () => {
   expect(gendiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(result);
 });
+
+test('check yaml stylish', () => {
+  expect(gendiff('__fixtures__/file1.yaml', '__fixtures__/file2.yaml')).toEqual(result);
+});
+
+test('check yml stylish', () => {
+  expect(gendiff('__fixtures__/file1.yml', '__fixtures__/file2.yml')).toEqual(result);
+});
