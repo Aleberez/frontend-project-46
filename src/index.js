aa2f8d88528b4ce3db219ffc94538efbbba4285b
lazfnol <lazfnol@gmail.com>
2025-05-09 02:53:32 +0300

    refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,33 +1,20 @@
 import { readFileSync } from 'node:fs';
 import path from 'node:path';
-import _ from 'lodash';
 import parse from './parsers.js';
+import getTree from './treeBuilder.js';
+import formatter from './formatters/index.js';
 
 const resolvePath = (filePath) => path.resolve(process.cwd(), filePath);
 
 const getExtension = (filename) => path.extname(filename).slice(1);
 
-const gendiff = (filepath1, filepath2) => {
+const gendiff = (filepath1, filepath2, format = 'stylish') => {
   const file1Content = readFileSync(resolvePath(filepath1), 'utf-8');
   const file2Content = readFileSync(resolvePath(filepath2), 'utf-8');
   const data1 = parse(file1Content, getExtension(filepath1));
   const data2 = parse(file2Content, getExtension(filepath2));
 
-  const keys = _.sortBy([...new Set([...Object.keys(data1), ...Object.keys(data2)])]);
-  const result = keys.map((key) => {
-    if (!Object.keys(data2).includes(key)) {
-      return `  - ${key}: ${data1[key]}`;
-    }
-    if (!Object.keys(data1).includes(key)) {
-      return `  + ${key}: ${data2[key]}`;
-    }
-    if (data1[key] !== data2[key]) {
-      return `  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}`;
-    }
-    return `    ${key}: ${data1[key]}`;
-  }).join('\n');
-
-  return `{\n${result}\n}`;
+  return formatter(getTree(data1, data2), format = 'stylish');
 };
 
 export default gendiff;
