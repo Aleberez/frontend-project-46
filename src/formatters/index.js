f18b7286ed333ae46302e3ca041602703cb107ee
lazfnol <lazfnol@gmail.com>
2025-05-09 05:18:06 +0300

    add flat format

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,9 +1,12 @@
 import stylish from './stylish.js';
+import makeFlat from './flat.js';
 
 export default function formatter(tree, format) {
   switch (format) {
     case 'stylish':
       return stylish(tree);
+    case 'flat':
+      return makeFlat(tree);
     default:
       throw new Error('Uncorrect data');
   }
