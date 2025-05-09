91e596ead279ac1b56e6cfc470b08f4d2c3ccea7
lazfnol <lazfnol@gmail.com>
2025-05-09 05:16:56 +0300

    add tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,14 +1,27 @@
 import gendiff from '../src/index.js';
 import result from '../__fixtures__/result.js';
+import resultFlat from '../__fixtures__/resultPlain.js';
 
 test('check json stylish', () => {
   expect(gendiff('__fixtures__/file1.json', '__fixtures__/file2.json', 'stylish')).toEqual(result);
 });
 
 test('check yaml stylish', () => {
-  expect(gendiff('__fixtures__/file1.yaml', '__fixtures__/file2.yaml')).toEqual(result);
+  expect(gendiff('__fixtures__/file1.yaml', '__fixtures__/file2.yaml', 'stylish')).toEqual(result);
 });
 
 test('check yml stylish', () => {
-  expect(gendiff('__fixtures__/file1.yml', '__fixtures__/file2.yml')).toEqual(result);
+  expect(gendiff('__fixtures__/file1.yml', '__fixtures__/file2.yml', 'stylish')).toEqual(result);
+});
+
+test('check json flat', () => {
+  expect(gendiff('__fixtures__/file1.json', '__fixtures__/file2.json', 'flat')).toEqual(resultFlat);
+});
+
+test('check yaml flat', () => {
+  expect(gendiff('__fixtures__/file1.yaml', '__fixtures__/file2.yaml', 'flat')).toEqual(resultFlat);
+});
+
+test('check yml flat', () => {
+  expect(gendiff('__fixtures__/file1.yml', '__fixtures__/file2.yml', 'flat')).toEqual(resultFlat);
 });
