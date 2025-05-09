5e8e327326a7f5d30c71432e832d68114da4c5cb
lazfnol <lazfnol@gmail.com>
2025-05-09 05:19:11 +0300

    lint fix

--- src/formatters/flat.js
+++ src/formatters/flat.js
@@ -1,5 +1,3 @@
-import _ from 'lodash';
-
 const formatValue = (value) => {
   if (value === null) return 'null';
   if (typeof value === 'object') return '[complex value]';
@@ -8,34 +6,32 @@ const formatValue = (value) => {
 };
 
 const makeFlat = (diffTree) => {
-  const buildLines = (nodes, parentPath = []) => {
-    return nodes.flatMap((node) => {
-      const currentPath = [...parentPath, node.key].join('.');
-      
-      switch (node.type) {
-        case 'nested':
-          return buildLines(node.children, [...parentPath, node.key]);
-          
-        case 'added':
-          return `Property '${currentPath}' was added with value: ${formatValue(node.value)}`;
-          
-        case 'removed':
-          return `Property '${currentPath}' was removed`;
-          
-        case 'changed':
-          return `Property '${currentPath}' was updated. From ${formatValue(node.oldValue)} to ${formatValue(node.newValue)}`;
-          
-        case 'unchanged':
-          return null;
-          
-        default:
-          throw new Error(`Unknown node type: ${node.type}`);
-      }
-    });
-  };
+  const buildLines = (nodes, parentPath = []) => nodes.flatMap((node) => {
+    const currentPath = [...parentPath, node.key].join('.');
+
+    switch (node.type) {
+      case 'nested':
+        return buildLines(node.children, [...parentPath, node.key]);
+
+      case 'added':
+        return `Property '${currentPath}' was added with value: ${formatValue(node.value)}`;
+
+      case 'removed':
+        return `Property '${currentPath}' was removed`;
+
+      case 'changed':
+        return `Property '${currentPath}' was updated. From ${formatValue(node.oldValue)} to ${formatValue(node.newValue)}`;
+
+      case 'unchanged':
+        return null;
+
+      default:
+        throw new Error(`Unknown node type: ${node.type}`);
+    }
+  });
 
   const lines = buildLines(diffTree.children);
-  return lines.filter(line => line !== null).join('\n');
+  return lines.filter((line) => line !== null).join('\n');
 };
 
 export default makeFlat;
